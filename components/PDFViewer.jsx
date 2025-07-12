55dda66efcd187ca6c0ba3a20db41b775e831681
bhavesh-gharat-kit <bhavesh.gharat.kit@gmail.com>
2025-07-12 13:15:17 +0530

    corrected PdfViewer to PDFViewer

--- components/PDFViewer.jsx
+++ components/PDFViewer.jsx
@@ -12,6 +12,7 @@ import {
 } from "react-icons/fa";
 // import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 // import "react-pdf/dist/esm/Page/TextLayer.css";
+//
 
 import workerSrc from "pdfjs-dist/build/pdf.worker.min.mjs?url";
 pdfjs.GlobalWorkerOptions.workerSrc = workerSrc;
